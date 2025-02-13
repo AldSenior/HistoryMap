dfbab87accde4373fbc4fcdffdf900695f5e079a
AldSenior <aldsio196@gmail.com>
2025-02-13 20:41:17 +0300

    verse1.33

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	basePath: "/HistoryMap",
+	output: "export",
+	images: {
+		unoptimized: true
+	}
+}
 
-export default nextConfig;
+export default nextConfig

0dc3ac76b6f0f538cd818be7a37dcd1380833fbc
AldSenior <aldsio196@gmail.com>
2025-03-08 15:40:59 +0300

    verse1.xz

--- src/app/components/Modal.js
+++ src/app/components/Modal.js
@@ -1,57 +1,73 @@
-import { motion } from 'framer-motion';
-import Slider from 'react-slick';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import { motion } from 'framer-motion'
+import ReactDOM from 'react-dom'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick-theme.css'
+import 'slick-carousel/slick/slick.css'
 
 function Modal({ isVisible, onClose, event }) {
-  if (!isVisible || !event || event.length === 0) return null; // Прекращаем рендеринг, если данные пустые
+	if (!isVisible || !event || event.length === 0) return null // Прекращаем рендеринг, если данные пустые
 
-  const modalVariants = {
-    hidden: { opacity: 0, y: -50 },
-    visible: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: -50 },
-  };
+	const modalVariants = {
+		hidden: { opacity: 0, y: -50 },
+		visible: { opacity: 1, y: 0 },
+		exit: { opacity: 0, y: -50 },
+	}
 
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+	const settings = {
+		dots: true,
+		infinite: false,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	}
 
-  return ReactDOM.createPortal(
-    <motion.div
-      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50" // Убедитесь, что здесь используется полупрозрачный черный фон
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-    >
-      <div className="bg-white max-w-[50vw] text-cyan-300 p-6 rounded-md shadow-md relative">
-        <button onClick={onClose} className="absolute top-2 right-2 text-gray-600">✖️</button>
-        <Slider {...settings}>
-          {event.map(e => (
-            <div key={e.id} className="p-4">
-              <h2 className="text-xl font-semibold">{e.title}</h2>
-              <p>{e.date}</p>
-              <p>{e.description}</p>
-              <div className="mt-2">
-                {e.personalities.map(person => (
-                  <div key={person.id} className="flex items-center mt-2">
-                    <img src={person.image} alt={person.name} className="w-8 h-8 rounded-full mr-2" />
-                    <span>{person.name}</span>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
-        </Slider>
-      </div>
-    </motion.div>,
-    document.body
-  );
+	return ReactDOM.createPortal(
+		<motion.div
+			className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50'
+			initial='hidden'
+			animate='visible'
+			exit='exit'
+		>
+			<div className='bg-white max-w-[50vw] text-cyan-300 p-6 rounded-md shadow-md relative'>
+				<button
+					onClick={onClose}
+					className='absolute top-2 right-2 text-gray-600'
+				>
+					✖️
+				</button>
+				<Slider {...settings}>
+					{event.map(e => (
+						<div key={e.id} className='p-4'>
+							<h2
+								className='text-xl font-semibold'
+								onClick={() => console.log(e)}
+							>
+								{e.title}
+							</h2>
+							<p>{e.date}</p>
+							<p>{e.description}</p>
+							<div className='mt-2'>
+								{e.personalities.map(person => (
+									<div
+										key={person.id}
+										className='flex items-center mt-2'
+										onClick={() => console.log(person)}
+									>
+										<img
+											src={person.image}
+											className='w-8 h-8 rounded-full mr-2'
+										/>{' '}
+										<span>{person.name}</span>
+									</div>
+								))}
+							</div>
+						</div>
+					))}
+				</Slider>
+			</div>
+		</motion.div>,
+		document.body
+	)
 }
 
-export default Modal;
+export default Modal
--- src/app/components/MyMapContainer.jsx
+++ src/app/components/MyMapContainer.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { AnimatePresence, motion } from 'framer-motion'
-import { useState, useRef, useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Modal from './Modal'
 
 function MyMapComponent({ events }) {
@@ -12,36 +12,35 @@ function MyMapComponent({ events }) {
 	const [hoveredMarkerId, setHoveredMarkerId] = useState(null)
 	const [scale, setScale] = useState(1)
 	const [translate, setTranslate] = useState({ x: 0, y: 0 })
-	const [isDragging, setIsDragging] = useState(false);
-	const [dragStart, setDragStart] = useState({ x: 0, y: 0 });
+	const [isDragging, setIsDragging] = useState(false)
+	const [dragStart, setDragStart] = useState({ x: 0, y: 0 })
 
-	const handleMouseDown = (e) => {
-		setIsDragging(true);
-		setDragStart({ x: e.clientX - translate.x, y: e.clientY - translate.y });
-	};
+	const handleMouseDown = e => {
+		setIsDragging(true)
+		setDragStart({ x: e.clientX - translate.x, y: e.clientY - translate.y })
+	}
 
-	const handleMouseMove = (e) => {
-		if (!isDragging) return;
-		const dx = e.clientX - dragStart.x;
-		const dy = e.clientY - dragStart.y;
-		const rect = containerRef.current.getBoundingClientRect();
-		const newTranslateX = (rect.width, dx);
+	const handleMouseMove = e => {
+		if (!isDragging) return
+		const dx = e.clientX - dragStart.x
+		const dy = e.clientY - dragStart.y
+		const rect = containerRef.current.getBoundingClientRect()
+		const newTranslateX = (rect.width, dx)
 		const newTranslateY = (rect.height, dy)
-		setTranslate({ x: newTranslateX, y: newTranslateY });
-	};
-
+		setTranslate({ x: newTranslateX, y: newTranslateY })
+	}
 
 	const handleMouseUp = () => {
-		setIsDragging(false);
-	};
-
+		setIsDragging(false)
+	}
 
 	const mapRef = useRef(null)
 	const containerRef = useRef(null)
 
-	const handleMarkerClick = (event) => {
+	const handleMarkerClick = event => {
 		const eventsAtCoordinates = filteredEvents.filter(
-			(e) => e.coords.lat === event.coords.lat && e.coords.lng === event.coords.lng
+			e =>
+				e.coords.lat === event.coords.lat && e.coords.lng === event.coords.lng
 		)
 		setSelectedEvent(eventsAtCoordinates)
 		setModalVisible(true)
@@ -52,18 +51,18 @@ function MyMapComponent({ events }) {
 		setSelectedEvent(null)
 	}
 
-	const filteredEvents = events.filter((event) =>
+	const filteredEvents = events.filter(event =>
 		event.title.toLowerCase().includes(searchTerm.toLowerCase())
 	)
 
-	const handleMapClick = (e) => {
+	const handleMapClick = e => {
 		const bounds = e.currentTarget.getBoundingClientRect()
 		const x = e.clientX - bounds.left
 		const y = e.clientY - bounds.top
 		setClickedCoordinates({ x, y })
 	}
 
-	const handleWheel = (e) => {
+	const handleWheel = e => {
 		e.preventDefault()
 		const delta = e.deltaY > 0 ? 0.9 : 1.1
 		const rect = containerRef.current.getBoundingClientRect()
@@ -71,8 +70,20 @@ function MyMapComponent({ events }) {
 		const mouseY = e.clientY - rect.top
 
 		const newScale = Math.max(0.5, Math.min(2, scale * delta))
-		const newTranslateX = Math.max(0, Math.min(rect.width - rect.width * newScale, mouseX - (mouseX - translate.x) * (newScale / scale)))
-		const newTranslateY = Math.max(0, Math.min(rect.height - rect.height * newScale, mouseY - (mouseY - translate.y) * (newScale / scale)))
+		const newTranslateX = Math.max(
+			0,
+			Math.min(
+				rect.width - rect.width * newScale,
+				mouseX - (mouseX - translate.x) * (newScale / scale)
+			)
+		)
+		const newTranslateY = Math.max(
+			0,
+			Math.min(
+				rect.height - rect.height * newScale,
+				mouseY - (mouseY - translate.y) * (newScale / scale)
+			)
+		)
 
 		setScale(newScale)
 		setTranslate({ x: newTranslateX, y: newTranslateY })
@@ -99,24 +110,23 @@ function MyMapComponent({ events }) {
 	}, [isDragging, scale, translate])
 
 	return (
-		<div className="flex w-screen h-screen flex-col items-center justify-center bg-gray-100">
+		<div className='flex w-screen h-screen flex-col items-center justify-center bg-gray-100'>
 			<input
-				type="text"
-				placeholder="Поиск событий..."
-				className="mb-4 p-3 border border-gray-300 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+				type='text'
+				placeholder='Поиск событий...'
+				className='mb-4 p-3 border border-gray-300 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400'
 				value={searchTerm}
-				onChange={
-					(e) => setSearchTerm(e.target.value)}
+				onChange={e => setSearchTerm(e.target.value)}
 			/>
 
 			<div
 				ref={containerRef}
-				className="relative w-4/5 h-4/5 bg-gray-200 overflow-hidden rounded-lg shadow-lg"
+				className='relative w-4/5 h-4/5 bg-gray-200 overflow-hidden rounded-lg shadow-lg'
 			>
 				<div
-					id="map"
+					id='map'
 					ref={mapRef}
-					className="absolute w-[1600px] h-[900px] bg-cover bg-center cursor-grab"
+					className='absolute w-[1600px] h-[900px] bg-cover bg-center cursor-grab'
 					onClick={handleMapClick}
 					onMouseDown={handleMouseDown}
 					onMouseMove={handleMouseMove}
@@ -127,16 +137,17 @@ function MyMapComponent({ events }) {
 					}}
 				>
 					<AnimatePresence>
-						{filteredEvents.map((event) => (
+						{filteredEvents.map(event => (
 							<motion.div
 								key={event.id}
 								onClick={() => handleMarkerClick(event)}
-								className={`absolute cursor-pointer rounded-full w-7 h-7 bg-blue-500 ${hoveredMarkerId === event.id ? 'scale-150' : ''
-									}`}
-								id="marker"
+								className={`absolute cursor-pointer rounded-full w-7 h-7 bg-blue-500 ${
+									hoveredMarkerId === event.id ? 'scale-150' : ''
+								}`}
+								id='marker'
 								style={{
-									left: `${((event.coords.lng / 360) + 0.5) * 100}%`,
-									top: `${((event.coords.lat / 180) + 0.5) * 100}%`,
+									left: `${(event.coords.lng / 360 + 0.5) * 100}%`,
+									top: `${(event.coords.lat / 180 + 0.5) * 100}%`,
 								}}
 								initial={{ scale: 0 }}
 								animate={{ scale: hoveredMarkerId === event.id ? 1.5 : 1 }}
@@ -150,7 +161,7 @@ function MyMapComponent({ events }) {
 
 					{clickedCoordinates && (
 						<motion.div
-							className="absolute bg-white border border-gray-300 p-2 rounded-md shadow-md"
+							className='absolute bg-white border border-gray-300 p-2 rounded-md shadow-md'
 							style={{ left: clickedCoordinates.x, top: clickedCoordinates.y }}
 							initial={{ opacity: 0, scale: 0 }}
 							animate={{ opacity: 1, scale: 1 }}
@@ -164,7 +175,11 @@ function MyMapComponent({ events }) {
 				</div>
 			</div>
 
-			<Modal isVisible={modalVisible} onClose={closeModal} event={selectedEvent} />
+			<Modal
+				isVisible={modalVisible}
+				onClose={closeModal}
+				event={selectedEvent}
+			/>
 		</div>
 	)
 }
